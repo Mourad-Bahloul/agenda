0c93e14b04b3c5fea682b510347d66adb6591355
Mourad BAHLOUL <mourad.bahloul@acensi.fr>
2023-06-05 20:38:13 +0200

    première version : conf de jpa

---
+++ src/main/java/com/hc/agenda/entities/Role.java
@@ -0,0 +1,25 @@
+package com.hc.agenda.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "hc_role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    // Define role fields and relationships
+
+    // Getters and setters
+}
---
+++ src/main/java/com/hc/agenda/repositories/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.hc.agenda.repositories;
+
+import com.hc.agenda.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
